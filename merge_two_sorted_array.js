536cf6b8e6cb45756baa5a2777da57a9e3f13956
Himadri Chowdhury <himadrichowdhury172@gmail.com>
2022-05-28 18:06:15 +0600

    Create merge_two_sorted_array.js

---
+++ merge_two_sorted_array.js
@@ -0,0 +1,33 @@
+merge two sorted arrays in one sorted array
+
+
+function merge(a, b) {
+    var result = [];
+    var ai = 0;
+    var bi = 0;
+    while (true) {
+        if ( ai < a.length && bi < b.length) {
+            if (a[ai] < b[bi]) {
+                result.push(a[ai]);
+                ai++;
+            } else if (a[ai] > b[bi]) {
+                result.push(b[bi]);
+                bi++;
+            } else {
+                result.push(a[ai]);
+                result.push(b[bi]);
+                ai++;
+                bi++;
+            }
+        } else if (ai < a.length) {
+            result.push.apply(result, a.slice(ai, a.length));
+            break;
+        } else if (bi < b.length) {
+            result.push.apply(result, b.slice(bi, b.length));
+            break;
+        } else {
+            break;
+        }
+    }
+    return result;
+}
